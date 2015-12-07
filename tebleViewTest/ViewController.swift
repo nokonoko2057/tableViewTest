9ef5d3b6f376ccd500a51add3edc0b87f49dd3c7
豁ｦ莠戊｣戊ｲｴ <nokonoko2057@gmail.com>
2015-12-07 20:31:24 +0900

    Initial Commit

---
+++ tebleViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tebleViewTest
+//
+//  Created by yuki takei on 2015/12/07.
+//  Copyright © 2015年 yuki takei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
